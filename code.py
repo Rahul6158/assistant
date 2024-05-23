55c32e92b6fafcc427c38cae5b5ba52d614397b4
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2024-05-23 13:15:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,5 +2,5 @@ from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 import av
 import cv2
 
-webrtc_streamer(key="example", video_transformer_factory=None)
+webrtc_streamer(key= 1, video_transformer_factory=None)
 
